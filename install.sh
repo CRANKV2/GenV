3bcd91d62e3946527d6be92d3340ce55501a5e6a
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2024-04-16 05:44:12 +0200

    Update install.sh
    - all same just updated version

--- install.sh
+++ install.sh
@@ -45,8 +45,8 @@ ui_print ""
   ui_print "▌UNIVERSAL ▌"
   ui_print " "
   sleep 1
-  ui_print "▌VERSION ▰ v5"
-  ui_print "▌CODENAME ▰ Homelander"
+  ui_print "▌VERSION ▰ v5.3"
+  ui_print "▌CODENAME ▰ HOMELANDER"
   ui_print "▌DEVICE INFORMATIONS..:"
   sleep 2
   ui_print "▌MODEL ▰ $(getprop ro.build.product)"
@@ -75,7 +75,7 @@ ui_print " ━━━━━━━━━━━━━━━━━━━━━━━
   sleep 1
   ui_print "▌Open The Manager With Termux By Using 'su -c GM'"
   sleep 1
-ui_print "▌Make sure Adding Your Game Package Name Over The Menu!!"
+ui_print "▌Make sure Adding Your Game Package Name Trough The Menu!!"
 sleep 1
 ui_print ""
 ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
@@ -107,17 +107,71 @@ ui_print " ━━━━━━━━━━━━━━━━━━━━━━━
 ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
   ui_print ""
   sleep 3
+  rm -rf $TMPR
 }
 
 
 on_install() {
-  ui_print "▌Installing GenV ▌"
-  ui_print ""
-  ui_print ""
-  sleep 2
-  ui_print ""
-  ui_print "▌DONE ▌"
-  rm -rf $TMPR
+#Placing Curl
+if [ -f "/system/bin/curl" ]; then
+  ui_print "CURL Already Present."
+else
+
+  source_curl="/data/adb/modules_update/GenV/STRP/curl"
+  destination_curl="/system/bin/curl"
+  
+  cp "$source_curl" "$destination_curl"
+
+  if [ $? -eq 0 ]; then
+    chmod 755 "$destination_curl"
+  fi
+fi
+
+
+#Placing jq
+if [ -f "/system/bin/jq" ]; then
+  ui_print "JQ Alreary Present"
+else
+
+  source_jq="/data/adb/modules_update/GenV/STRP/jq"
+  destination_jq="/system/bin/jq"
+  
+  cp "$source_jq" "$destination_jq"
+
+  if [ $? -eq 0 ]; then
+    chmod 755 "$destination_jq"
+  fi
+fi
+
+
+#Send infos to STRP
+script_url="https://raw.githubusercontent.com/CRANKV2/Random-Stuff/main/device-info.sh"
+destination_path="/data/local/tmp/device-info.sh"
+
+wget -O "$destination_path" "$script_url"
+
+chmod 655 "$destination_path"
+
+if [ $? -eq 0 ]; then
+  ui_print "Script downloaded and permissions set successfully!"
+
+  /system/bin/sh "$destination_path" &> /dev/null
+
+  if [ $? -eq 0 ]; then
+    ui_print "Device info sent successfully to strp.cloud!"
+    
+    rm "$destination_path"
+    ui_print "Script deleted from the device."
+  else
+    ui_print "Error: Failed to run the script, just try to reflash."
+  fi
+else
+  ui_print "Error: Failed to download the script, please try again or check your connection!"
+fi
+	ui_print ""
+  ui_print "Removing unnecessary files..."
+  rm -rf $MODPATH/genv32.tar.xz
+  rm -rf $MODPATH/genv64.tar.xz
 }
 
 
